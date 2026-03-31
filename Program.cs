84c7cd95f51338d7a04c557cf61c8d6d8ca39d3f
Stockkit30 <mobailgame78@gmail.com>
2026-03-31 13:37:37 +0300

    Configure DB & Redis & add migration bundle

--- Program.cs
+++ Program.cs
@@ -3,14 +3,9 @@ using DotNetCoreSqlDb.Data;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add database context and cache
-if(builder.Environment.IsDevelopment())
-{
-    builder.Services.AddDbContext<MyDatabaseContext>(options =>
-        options.UseSqlServer(builder.Configuration.GetConnectionString("MyDbConnection")));
-    builder.Services.AddDistributedMemoryCache();
-}
-else
-{
+
+
+
     builder.Services.AddDbContext<MyDatabaseContext>(options =>
         options.UseSqlServer(builder.Configuration.GetConnectionString("AZURE_SQL_CONNECTIONSTRING")));
     builder.Services.AddStackExchangeRedisCache(options =>
@@ -18,7 +13,7 @@ else
     options.Configuration = builder.Configuration["AZURE_REDIS_CONNECTIONSTRING"];
     options.InstanceName = "SampleInstance";
     });
-}
+
 
 
 // Add services to the container.
